be06c5571947957fc1cc519f71ed0b63ba4b15c3
Bayardo <bjlslopez@gmail.com>
2020-11-03 11:54:33 -0600

    Controller de Cliente creado. No funcional

---
+++ WebCRUDapi/Models/ServiciosCLS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Web;
+
+namespace WebCRUDapi.Models
+{
+    public class ServiciosCLS
+    {
+        public int IdServicio { get; set; }
+        public int IdTipoServicio { get; set; }
+        public string Nombre { get; set; }
+        public SqlMoney CostoBase { get; set; }
+
+    }
+}
