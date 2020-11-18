b50cc6b0d5484cc60f6b28f4788ae7dc90f13cef
Bayardo <bjlslopez@gmail.com>
2020-11-18 14:20:44 -0600

    Terminado el CRUD de la tabla de Ubicaciones

---
+++ WebCRUDapi/Models/UbicacionCLS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebCRUDapi.Models
+{
+    public class UbicacionCLS
+    {
+        public int IdUbicacion { get; set; }
+        public int IdMaestro { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+    }
+}
